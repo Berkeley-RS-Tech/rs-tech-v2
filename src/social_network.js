288e12c48cec5a3ca35836b591bf1898a46af062
PingMyBug <86208414+PingMyBug@users.noreply.github.com>
2022-08-01 10:54:12 -0700

    As example

---
+++ src/social_network.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React from 'react';
+import owl from './reentry-owl.jpg';
+
+
+function openInNewTab(url) {
+  window.open(url, '_blank', 'noopener,noreferrer');
+}
+
+function social_network() {
+  return (
+
+      <div className="owl-logo">
+      <a botton onClick={() => openInNewTab("https://www.youtube.com/watch?v=dQw4w9WgXcQ")} > 
+        <img src={owl} alt='owl'/>
+        </a>
+      </div>
+
+  );
+}
+
+export default social_network;
